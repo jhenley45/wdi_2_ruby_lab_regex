1ae1fc778329b8354454ae24484a20edb346427b
Jack Henley <jack.d.henley@gmail.com>
2014-01-14 15:39:50 -0500

    about halfway there

--- main.rb
+++ main.rb
@@ -1,26 +1,32 @@
 require_relative 'source'
 require 'pry'
 
-def valid_ip_addresses 
-end 
-
-def valid_mit_ip_addresses 
+def valid_ip_addresses(string)
+	string.scan(/\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}/)
 end
 
-def non_mit_ip_addresses 
-end 
-
-def valid_phone_numbers 
+def valid_mit_ip_addresses(string)
+	string.scan(/18[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}/)
 end
 
-def area_codes 
+def non_mit_ip_addresses(string)
+	string.scan(/\d{3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}/)
 end
 
-def email_addresses 
+def valid_phone_numbers(string)
+	string.scan(/\d{3}[\-]\d{3}[\-]\d{4}/)
 end
 
-def zip_codes 
+def area_codes(string)
+	string.scan(//)
 end
 
-def hex_colors 
+def email_addresses(string)
+end
+
+def zip_codes(string)
+	string.scan(/[0][2]\d{3}/)
+end
+
+def hex_colors(string)
 end
